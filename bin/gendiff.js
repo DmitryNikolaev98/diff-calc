5810cac7fa44fa640b01f26a91a8740cd3b86694
Dmitry Nikolaev <nik.dima001@yandex.ru>
2023-01-03 14:14:13 +0300

    added arguments and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,8 @@ const program = new Command();
 program.name('gendiff')
   .version('1.0.0', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>');
 
 program.parse();
