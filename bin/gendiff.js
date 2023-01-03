6662ed3fcd2059288ab93e4ac18d747f0a86ce2a
Dmitry Nikolaev <nik.dima001@yandex.ru>
2023-01-03 15:18:59 +0300

    Comparison of flat files (JSON)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-const { Command } = require('commander');
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
 program.name('gendiff')
   .version('1.0.0', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> <filepath2>');
-
-program.parse();
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
+  .parse();
