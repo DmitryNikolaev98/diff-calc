6367dad66ea35d0d69a57a4998e807c16ad96656
Dmitry Nikolaev <nik.dima001@yandex.ru>
2023-01-05 00:59:10 +0300

    recursive comparison

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import fs from 'fs';
 import path from 'node:path';
 import getDifference from './getDifference.js';
+import parse from './parser.js';
+import stylish from './formaters/stylish.js';
 
 const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
-const getFile = (absolutePath) => fs.readFileSync(absolutePath);
 
 export default (filepath1, filepath2) => {
   const path1 = getAbsolutePath(filepath1);
   const path2 = getAbsolutePath(filepath2);
-  const data1 = JSON.parse(getFile(path1));
-  const data2 = JSON.parse(getFile(path2));
-  return getDifference(data1, data2);
+  const data1 = parse(path1);
+  const data2 = parse(path2);
+  return stylish(getDifference(data1, data2));
 };
