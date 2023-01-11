100a3cd87e06f6cc19e9f9a0ffc8b830bcb3545b
Dmitry <nik.dima001@yandex.ru>
2023-01-11 14:02:23 +0300

    fix index.test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 describe.each([
-  ['stylish', 'result.stylish.txt'],['plain', 'result.plain.txt'],
+  ['stylish', 'result.stylish.txt'], ['plain', 'result.plain.txt'],
   ['json', 'result.json.txt'],
 ])('format %s', (format, expected) => {
   test.each([
